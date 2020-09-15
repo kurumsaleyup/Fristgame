5800cde168188ba25032ba2ee6ef3f88546f9f91
kurumsaleyup <kurumsaleyup@users.noreply.github.com>
2020-09-15 14:56:50 +0300

    FristPersonalGame with GitHub
    Just to make sure that I can do it.

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float fowardForce = 200f;
+    public float sidewayForce = 500f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //add foward force
+        rb.AddForce(0, 0, fowardForce * Time.deltaTime);
+
+        //to move right and left
+        if (Input.GetKey("d")){
+            rb.AddForce(sidewayForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a")){
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
